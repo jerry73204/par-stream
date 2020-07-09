20061b7152178b8fc2cc2cceac533cababec7e76
jerry73204 <jerry73204@gmail.com>
2020-07-10 01:47:45 +0800

    Verbose doc comments with examples

--- src/future.rs
+++ src/future.rs
@@ -1,6 +1,29 @@
 use crate::common::*;
 
 /// An extension trait for [Future](Future) that provides combinator functions for parallelism.
+///
+/// It simplifies the writing of spawning a parallel task. The following code are equivalent.
+///
+/// ```rust
+/// use futures::stream::StreamExt;
+/// use par_stream::ParFuture;
+///
+/// #[async_std::main]
+/// async fn main() {
+///     // original
+///     async_std::task::spawn(async move {
+///         println!("a parallel task");
+///     })
+///     .await;
+///
+///     // using the method
+///     async move {
+///         println!("a parallel task");
+///     }
+///         .spawned()
+///         .await;
+/// }
+/// ```
 pub trait ParFuture {
     /// Spawn a task that polls the given future.
     fn spawned<T>(self) -> JoinHandle<T>
--- src/stream.rs
+++ src/stream.rs
@@ -1,6 +1,32 @@
+//! futures-compatible parallel stream extension.
+
 use crate::common::*;
 
 /// Collect multiple streams into single stream.
+///
+/// ```rust
+/// use futures::stream::StreamExt;
+/// use par_stream::ParStreamExt;
+/// use std::collections::HashSet;
+///
+/// #[async_std::main]
+/// async fn main() {
+///     let outer = Box::new(2);
+///
+///     // scatter to two receivers
+///     let (scatter_fut, rx1) = futures::stream::iter(0..1000).par_scatter(None);
+///     let rx2 = rx1.clone();
+///
+///     // gather back from two receivers
+///     let gather_fut = par_stream::par_gather(vec![rx1, rx2], None).collect::<HashSet<_>>();
+///
+///     // collect the items from respective workers
+///     let ((), values) = futures::join!(scatter_fut, gather_fut);
+///
+///     // the gathered values have exactly the same size with the stream
+///     assert_eq!(values, (0..1000).collect::<HashSet<_>>());
+/// }
+/// ```
 pub fn par_gather<S>(
     streams: impl IntoIterator<Item = S>,
     buf_size: impl Into<Option<usize>>,
@@ -34,6 +60,35 @@ pub trait ParStreamExt {
     /// The `limit` is the number of parallel workers.
     /// If it is `0` or `None`, it defaults the number of cores on system.
     /// The method guarantees the order of output items obeys that of input items.
+    ///
+    /// Each parallel task runs in two-stage manner. The `f` closure is invoked in the
+    /// main thread and lets you clone over outer varaibles. Then, `f` returns a future
+    /// and the future will be sent to a parallel worker.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     let outer = Box::new(2);
+    ///
+    ///     let doubled = futures::stream::iter(0..1000)
+    ///         // doubles the values in parallel up to maximum number of cores
+    ///         .par_then(None, move |value| {
+    ///             // cloned needed variables in the main thread
+    ///             let cloned_outer = outer.clone();
+    ///
+    ///             // the future is sent to a parallel worker
+    ///             async move { value * (*cloned_outer) }
+    ///         })
+    ///         // the collected values will be ordered
+    ///         .collect::<Vec<_>>()
+    ///         .await;
+    ///     let expect = (0..1000).map(|value| value * 2).collect::<Vec<_>>();
+    ///     assert_eq!(doubled, expect);
+    /// }
+    /// ```
     fn par_then<T, F, Fut>(mut self, limit: impl Into<Option<usize>>, mut f: F) -> ParMap<T>
     where
         T: 'static + Send,
@@ -108,6 +163,36 @@ pub trait ParStreamExt {
     /// The `limit` is the number of parallel workers.
     /// If it is `0` or `None`, it defaults the number of cores on system.
     /// The order of output items is not guaranteed to respect the order of input items.
+    ///
+    /// Each parallel task runs in two-stage manner. The `f` closure is invoked in the
+    /// main thread and lets you clone over outer varaibles. Then, `f` returns a future
+    /// and the future will be sent to a parallel worker.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    /// use std::collections::HashSet;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     let outer = Box::new(2);
+    ///
+    ///     let doubled = futures::stream::iter(0..1000)
+    ///         // doubles the values in parallel up to maximum number of cores
+    ///         .par_then_unordered(None, move |value| {
+    ///             // clone needed variables in the main thread
+    ///             let cloned_outer = outer.clone();
+    ///
+    ///             // the future is sent to a parallel worker
+    ///             async move { value * (*cloned_outer) }
+    ///         })
+    ///         // the collected values may NOT be ordered
+    ///         .collect::<HashSet<_>>()
+    ///         .await;
+    ///     let expect = (0..1000).map(|value| value * 2).collect::<HashSet<_>>();
+    ///     assert_eq!(doubled, expect);
+    /// }
+    /// ```
     fn par_then_unordered<T, F, Fut>(
         mut self,
         limit: impl Into<Option<usize>>,
@@ -162,6 +247,36 @@ pub trait ParStreamExt {
     /// The `limit` is the number of parallel workers.
     /// If it is `0` or `None`, it defaults the number of cores on system.
     /// The method guarantees the order of output items obeys that of input items.
+    ///
+    /// Each parallel task runs in two-stage manner. The `f` closure is invoked in the
+    /// main thread and lets you clone over outer varaibles. Then, `f` returns a closure
+    /// and the closure will be sent to a parallel worker.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     // the variable will be shared by parallel workers
+    ///     let outer = Box::new(2);
+    ///
+    ///     let doubled = futures::stream::iter(0..1000)
+    ///         // doubles the values in parallel up to maximum number of cores
+    ///         .par_map(None, move |value| {
+    ///             // clone needed variables in the main thread
+    ///             let cloned_outer = outer.clone();
+    ///
+    ///             // the closure is sent to parallel worker
+    ///             move || value * (*cloned_outer)
+    ///         })
+    ///         // the collected values may NOT be ordered
+    ///         .collect::<Vec<_>>()
+    ///         .await;
+    ///     let expect = (0..1000).map(|value| value * 2).collect::<Vec<_>>();
+    ///     assert_eq!(doubled, expect);
+    /// }
+    /// ```
     fn par_map<T, F, Func>(self, limit: impl Into<Option<usize>>, mut f: F) -> ParMap<T>
     where
         T: 'static + Send,
@@ -180,6 +295,37 @@ pub trait ParStreamExt {
     /// The `limit` is the number of parallel workers.
     /// If it is `0` or `None`, it defaults the number of cores on system.
     /// The method guarantees the order of output items obeys that of input items.
+    ///
+    /// Each parallel task runs in two-stage manner. The `f` closure is invoked in the
+    /// main thread and lets you clone over outer varaibles. Then, `f` returns a future
+    /// and the future will be sent to a parallel worker.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    /// use std::collections::HashSet;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     // the variable will be shared by parallel workers
+    ///     let outer = Box::new(2);
+    ///
+    ///     let doubled = futures::stream::iter(0..1000)
+    ///         // doubles the values in parallel up to maximum number of cores
+    ///         .par_map_unordered(None, move |value| {
+    ///             // clone needed variables in the main thread
+    ///             let cloned_outer = outer.clone();
+    ///
+    ///             // the closure is sent to parallel worker
+    ///             move || value * (*cloned_outer)
+    ///         })
+    ///         // the collected values may NOT be ordered
+    ///         .collect::<HashSet<_>>()
+    ///         .await;
+    ///     let expect = (0..1000).map(|value| value * 2).collect::<HashSet<_>>();
+    ///     assert_eq!(doubled, expect);
+    /// }
+    /// ```
     fn par_map_unordered<T, F, Func>(
         self,
         limit: impl Into<Option<usize>>,
@@ -205,7 +351,27 @@ pub trait ParStreamExt {
     /// The `buf_size` is the size of buffer that stores the temporary reduced values.
     /// If it is `0` or `None`, it defaults the number of cores on system.
     ///
-    /// Unlike [StreamExt::fold], the method may not combine the values sequentially.
+    /// Unlike [StreamExt::fold], the method does not combine the values sequentially.
+    /// Instead, the parallel workers greedly take two values from the buffer, reduce to
+    /// one value, and push back to the buffer.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     // the variable will be shared by parallel workers
+    ///     let sum = futures::stream::iter(1..=1000)
+    ///         // sum up the values in parallel
+    ///         .par_reduce(None, None, move |lhs, rhs| {
+    ///             // the closure is sent to parallel worker
+    ///             async move { lhs + rhs }
+    ///         })
+    ///         .await;
+    ///     assert_eq!(sum, (1 + 1000) * 1000 / 2);
+    /// }
+    /// ```
     fn par_reduce<F, Fut>(
         mut self,
         limit: impl Into<Option<usize>>,
@@ -320,6 +486,50 @@ pub trait ParStreamExt {
     /// `routing_fn` is executed on the calling thread.
     ///
     /// `map_fns` is a vector of mapping functions, each of which produces an asynchronous closure.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    /// use std::{future::Future, pin::Pin};
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     let map_fns: Vec<Box<dyn FnMut(usize) -> Pin<Box<dyn Future<Output = usize> + Send>>>> = vec![
+    ///         // even number processor
+    ///         Box::new(|even_value| Box::pin(async move { even_value / 2 })),
+    ///         // odd number processor
+    ///         Box::new(|odd_value| Box::pin(async move { odd_value * 2 + 1 })),
+    ///     ];
+    ///
+    ///     let transformed = futures::stream::iter(0..1000)
+    ///         // doubles the values in parallel up to maximum number of cores
+    ///         .par_routing(
+    ///             None,
+    ///             move |value| {
+    ///                 // distribute the value according to its parity
+    ///                 if value % 2 == 0 {
+    ///                     0
+    ///                 } else {
+    ///                     1
+    ///                 }
+    ///             },
+    ///             map_fns,
+    ///         )
+    ///         // the collected values may NOT be ordered
+    ///         .collect::<Vec<_>>()
+    ///         .await;
+    ///     let expect = (0..1000)
+    ///         .map(|value| {
+    ///             if value % 2 == 0 {
+    ///                 value / 2
+    ///             } else {
+    ///                 value * 2 + 1
+    ///             }
+    ///         })
+    ///         .collect::<Vec<_>>();
+    ///     assert_eq!(transformed, expect);
+    /// }
+    /// ```
     fn par_routing<F1, F2, Fut, T>(
         mut self,
         buf_size: impl Into<Option<usize>>,
@@ -491,9 +701,40 @@ pub trait ParStreamExt {
 
     /// Reorder the input items paired with a iteration count.
     ///
-    /// The type of input item must be a tuple `(usize, T)`.
-    /// It reorders the items according to the first elemnt of tupple.
-    /// It is usually combined with [ParStreamExt::overflowing_enumerate].
+    /// The combinator asserts the input item has tuple type `(usize, T)`.
+    /// It reorders the items according to the first value of input tuple.
+    ///
+    /// It is usually combined with [ParStreamExt::overflowing_enumerate], then
+    /// applies a series of unordered parallel mapping, and finally reorders the values
+    /// back by this method. It avoids reordering the values after each parallel mapping step.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     let doubled = futures::stream::iter(0..1000)
+    ///         // add enumerated index that does not panic on overflow
+    ///         .overflowing_enumerate()
+    ///         // double the values in parallel
+    ///         .par_then_unordered(None, move |(index, value)| {
+    ///             // the closure is sent to parallel worker
+    ///             async move { (index, value * 2) }
+    ///         })
+    ///         // add values by one in parallel
+    ///         .par_then_unordered(None, move |(index, value)| {
+    ///             // the closure is sent to parallel worker
+    ///             async move { (index, value + 1) }
+    ///         })
+    ///         // reorder the values by enumerated index
+    ///         .reorder_enumerated()
+    ///         .collect::<Vec<_>>()
+    ///         .await;
+    ///     let expect = (0..1000).map(|value| value * 2 + 1).collect::<Vec<_>>();
+    ///     assert_eq!(doubled, expect);
+    /// }
+    /// ```
     fn reorder_enumerated<T>(self) -> ReorderEnumerated<T, Self>
     where
         Self: Stream<Item = (usize, T)> + Unpin + Sized,
@@ -505,7 +746,51 @@ pub trait ParStreamExt {
         }
     }
 
-    /// Splits the stream into a clonable receiver and a future that scatters input items into the receiver and its clones.
+    /// Splits the stream into a receiver and a future.
+    ///
+    /// The returned future scatters input items into the receiver and its clones,
+    /// and should be manually awaited by user.
+    ///
+    /// The returned receiver can be cloned and distributed to resepctive workers.
+    ///
+    /// It lets user to write custom workers that receive items from the same stream.
+    ///
+    /// ```rust
+    /// use futures::stream::StreamExt;
+    /// use par_stream::ParStreamExt;
+    ///
+    /// #[async_std::main]
+    /// async fn main() {
+    ///     let outer = Box::new(2);
+    ///
+    ///     let (scatter_fut, rx1) = futures::stream::iter(0..1000).par_scatter(None);
+    ///     let rx2 = rx1.clone();
+    ///
+    ///     // first parallel worker
+    ///     let worker1 = async_std::task::spawn(async move {
+    ///         let mut values = vec![];
+    ///         while let Ok(value) = rx1.recv().await {
+    ///             values.push(value);
+    ///         }
+    ///         values
+    ///     });
+    ///
+    ///     // second parallel worker
+    ///     let worker2 = async_std::task::spawn(async move {
+    ///         let mut values = vec![];
+    ///         while let Ok(value) = rx2.recv().await {
+    ///             values.push(value);
+    ///         }
+    ///         values
+    ///     });
+    ///
+    ///     // collect the items from respective workers
+    ///     let ((), values1, values2) = futures::join!(scatter_fut, worker1, worker2);
+    ///
+    ///     // the union of collected values have exactly the same size with the stream
+    ///     assert_eq!(values1.len() + values2.len(), 1000);
+    /// }
+    /// ```
     fn par_scatter(
         mut self,
         buf_size: impl Into<Option<usize>>,
