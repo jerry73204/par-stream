15c56fa676341a0d6863f15e79da9e3fdc29a92d
jerry73204 <jerry73204@gmail.com>
2021-10-11 04:08:24 +0800

    Add methods to divide a chunk into smaller chunks

--- src/slice.rs
+++ src/slice.rs
@@ -185,6 +185,11 @@ mod concurrent_chunks {
                 data: self.data.clone(),
             }
         }
+
+        /// Gets the reference count on the owning data.
+        pub fn ref_count(&self) -> usize {
+            Arc::strong_count(&self.data)
+        }
     }
 
     impl<S, T> Iterator for ConcurrentChunks<S, T>
@@ -232,6 +237,18 @@ mod concurrent_chunks {
         pub fn try_unwrap(self) -> Result<S, Self> {
             Arc::try_unwrap(self.data).map_err(|data| ConcurrentChunksGuard { data })
         }
+
+        pub fn unwrap(self) -> S
+        where
+            S: Debug,
+        {
+            self.try_unwrap().unwrap()
+        }
+
+        /// Gets the reference count on the owning data.
+        pub fn ref_count(&self) -> usize {
+            Arc::strong_count(&self.data)
+        }
     }
 }
 
@@ -250,11 +267,127 @@ mod chunk {
     }
 
     impl<S, T> Chunk<S, T> {
+        /// Splits the chunk into two sub-chunks, divided at specified index.
+        ///
+        /// # Panics
+        /// The method panics if the index is out of bound.
+        pub fn split_at(mut self, index: usize) -> (Chunk<S, T>, Chunk<S, T>)
+        where
+            S: AsMut<[T]>,
+            T: 'static + Send,
+        {
+            unsafe {
+                let data = self.data;
+                let slice: &mut [T] = self.slice.as_mut();
+                let lslice = NonNull::new_unchecked(&mut slice[0..index] as *mut [T]);
+                let rslice = NonNull::new_unchecked(&mut slice[index..] as *mut [T]);
+
+                (
+                    Chunk {
+                        data: data.clone(),
+                        slice: lslice,
+                    },
+                    Chunk {
+                        data,
+                        slice: rslice,
+                    },
+                )
+            }
+        }
+
+        /// Returns an iterator of roughly  fixed-sized chunks of the chunk.
+        ///
+        /// Each chunk has `chunk_size` elements, expect the last chunk maybe shorter
+        /// if there aren't enough elements.
+        ///
+        /// The yielded chunks maintain a global reference count on owning data. Each chunk refers to
+        /// a mutable and exclusive sub-slice, enabling concurrent processing on input data.
+        ///
+        /// # Panics
+        /// The method panics if `chunk_size` is zero and slice length is not zero.
+        pub fn chunks(self, chunk_size: usize) -> ConcurrentChunks<S, T>
+        where
+            S: 'static + AsMut<[T]> + Sized + Send,
+            T: 'static + Send,
+        {
+            unsafe {
+                let data = self.data;
+                let data_ptr = Arc::as_ptr(&data) as *mut S;
+                let data_slice = data_ptr.as_mut().unwrap().as_mut();
+
+                let slice_len = self.slice.as_ref().len();
+                let slice_ptr = self.slice.as_ref().as_ptr();
+                let start = slice_ptr.offset_from(data_slice.as_ptr()) as usize;
+
+                assert!(
+                    slice_len == 0 || chunk_size > 0,
+                    "chunk_size must be positive for non-empty slice"
+                );
+
+                ConcurrentChunks {
+                    chunk_size,
+                    index: start,
+                    end: start + slice_len,
+                    data,
+                    _phantom: PhantomData,
+                }
+            }
+        }
+
+        /// Returns an iterator of roughly `division` roughly fixed-sized chunks of the chunk.
+        ///
+        /// The chunk size is determined by `division`. The last chunk maybe shorter if
+        /// there aren't enough elements. If `division` is `None`, it defaults to
+        /// the number of system processors.
+        ///
+        /// # Panics
+        /// The method panics if `division` is zero and slice length is not zero.
+        pub fn chunks_by_division(
+            self,
+            division: impl Into<Option<usize>>,
+        ) -> ConcurrentChunks<S, T>
+        where
+            S: 'static + AsMut<[T]> + Sized + Send,
+            T: 'static + Send,
+        {
+            unsafe {
+                let division = division.into().unwrap_or_else(|| num_cpus::get());
+
+                let data = self.data;
+                let data_ptr = Arc::as_ptr(&data) as *mut S;
+                let data_slice = data_ptr.as_mut().unwrap().as_mut();
+
+                let slice_len = self.slice.as_ref().len();
+                let slice_ptr = self.slice.as_ref().as_ptr();
+                let start = slice_ptr.offset_from(data_slice.as_ptr()) as usize;
+
+                let chunk_size = if slice_len == 0 {
+                    0
+                } else {
+                    assert!(division > 0, "division must be positive, but get zero");
+                    (slice_len + division - 1) / division
+                };
+
+                ConcurrentChunks {
+                    index: start,
+                    chunk_size,
+                    end: start + slice_len,
+                    data,
+                    _phantom: PhantomData,
+                }
+            }
+        }
+
         /// Returns the guard that is used to recover the owning data.
         pub fn guard(&self) -> ConcurrentChunksGuard<S> {
             ConcurrentChunksGuard {
                 data: self.data.clone(),
             }
+        }
+
+        /// Gets the reference count on the owning data.
+        pub fn ref_count(&self) -> usize {
+            Arc::strong_count(&self.data)
         }
 
         /// Concatenates contiguous chunks into one chunk.
@@ -308,6 +441,13 @@ mod chunk {
                 };
 
                 Chunk { data, slice }
+            }
+        }
+
+        pub fn into_arc_ref(self) -> ArcRef<S, [T]> {
+            unsafe {
+                let Self { data, slice } = self;
+                ArcRef::new(data).map(|_| slice.as_ref())
             }
         }
     }
@@ -422,5 +562,58 @@ mod tests {
     fn empty_concurrent_chunks_test() {
         assert_eq!([(); 0].concurrent_chunks(2).count(), 0);
         assert_eq!([(); 0].concurrent_chunks_by_division(None).count(), 0);
+    }
+
+    #[test]
+    fn split_at_test() {
+        let vec: Vec<_> = (0..16).collect();
+        let (lslice, rslice) = vec.concurrent_split_at(5);
+        assert!(izip!(&lslice, 0..5).all(|(&lhs, rhs)| lhs == rhs));
+        assert!(izip!(&rslice, 5..16).all(|(&lhs, rhs)| lhs == rhs));
+    }
+
+    #[test]
+    fn chunks_of_chunk_test() {
+        let owner: Vec<_> = (0..9).collect();
+        let mut chunks = owner.concurrent_chunks_by_division(2);
+
+        let chunk1 = chunks.next().unwrap();
+        let chunk2 = chunks.next().unwrap();
+        assert!(chunks.next().is_none());
+        drop(chunks);
+
+        let mut chunks = chunk1.chunks(3);
+        let chunk3 = chunks.next().unwrap();
+        let chunk4 = chunks.next().unwrap();
+        assert!(chunks.next().is_none());
+        assert_eq!(&*chunk3, &[0, 1, 2]);
+        assert_eq!(&*chunk4, &[3, 4]);
+        drop(chunks);
+
+        let mut chunks = chunk2.chunks_by_division(3);
+        let chunk5 = chunks.next().unwrap();
+        let chunk6 = chunks.next().unwrap();
+        assert!(chunks.next().is_none());
+        assert_eq!(&*chunk5, &[5, 6]);
+        assert_eq!(&*chunk6, &[7, 8]);
+
+        let chunk7 = Chunk::cat(vec![chunk4, chunk5]);
+        assert_eq!(&*chunk7, &[3, 4, 5, 6]);
+
+        let (chunk8, chunk9) = chunk7.split_at(1);
+        assert_eq!(&*chunk8, &[3]);
+        assert_eq!(&*chunk9, &[4, 5, 6]);
+
+        // if the ref count is correct, the data should be recovered
+        let guard = chunk6.guard();
+
+        drop(chunks);
+        drop(chunk3);
+        drop(chunk6);
+        drop(chunk8);
+        drop(chunk9);
+
+        let owner = guard.try_unwrap().unwrap();
+        assert_eq!(owner, (0..9).collect::<Vec<_>>());
     }
 }
