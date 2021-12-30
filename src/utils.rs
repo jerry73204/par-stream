4c34c0dd12bfb1ed7a9eb3c558eb2ccae5365648
aeon <jerry73204@gmail>
2021-12-31 07:19:43 +0800

    Remove stream.par_routing() that is replaced by stream.pull_routing()

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use crate::common::*;
+// use crate::common::*;
 
 pub fn channel<T>(capacity: impl Into<Option<usize>>) -> (flume::Sender<T>, flume::Receiver<T>) {
     match capacity.into() {
@@ -7,31 +7,31 @@ pub fn channel<T>(capacity: impl Into<Option<usize>>) -> (flume::Sender<T>, flum
     }
 }
 
-pub fn join_future_stream<F, S>(future: F, stream: S) -> impl Stream<Item = S::Item>
-where
-    F: Future,
-    S: Stream,
-{
-    stream::select(future.map(|_| None).into_stream(), stream.map(Some))
-        .filter_map(|item| future::ready(item))
-}
+// pub fn join_future_stream<F, S>(future: F, stream: S) -> impl Stream<Item = S::Item>
+// where
+//     F: Future,
+//     S: Stream,
+// {
+//     stream::select(future.map(|_| None).into_stream(), stream.map(Some))
+//         .filter_map(|item| future::ready(item))
+// }
 
-pub use tokio_mpsc_receiver_ext::*;
+// pub use tokio_mpsc_receiver_ext::*;
 
-mod tokio_mpsc_receiver_ext {
-    use tokio::sync::mpsc;
-    use tokio_stream::wrappers::ReceiverStream;
+// mod tokio_mpsc_receiver_ext {
+//     use tokio::sync::mpsc;
+//     use tokio_stream::wrappers::ReceiverStream;
 
-    pub trait TokioMpscReceiverExt<T> {
-        fn into_stream(self) -> ReceiverStream<T>;
-    }
+//     pub trait TokioMpscReceiverExt<T> {
+//         fn into_stream(self) -> ReceiverStream<T>;
+//     }
 
-    impl<T> TokioMpscReceiverExt<T> for mpsc::Receiver<T>
-    where
-        T: 'static + Send,
-    {
-        fn into_stream(self) -> ReceiverStream<T> {
-            ReceiverStream::new(self)
-        }
-    }
-}
+//     impl<T> TokioMpscReceiverExt<T> for mpsc::Receiver<T>
+//     where
+//         T: 'static + Send,
+//     {
+//         fn into_stream(self) -> ReceiverStream<T> {
+//             ReceiverStream::new(self)
+//         }
+//     }
+// }
